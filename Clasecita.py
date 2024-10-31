404159b4d328a231499c5e16a77001693617265f
Juandaceballos <127470068+Juandaceballos@users.noreply.github.com>
2024-10-30 19:25:30 -0500

    Create Clasecita.py
    crear una clase para una aplicacion de citas.

---
+++ Clasecita.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+class Cita:
+    def __init__(self, usuario1, usuario2, fecha, lugar, descripcion):
+      
+        self.usuario1 = usuario1
+        self.usuario2 = usuario2
+        self.fecha = fecha
+        self.lugar = lugar
+        self.descripcion = descripcion
+
+   
