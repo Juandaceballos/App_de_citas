faa59e49257d28f4e8d9c5969aa315b84ef8abdb
Juandaceballos <127470068+Juandaceballos@users.noreply.github.com>
2024-10-30 19:27:49 -0500

    Create clasenodocita.py
    la clase para el nodo de citas de la aplicacion de citas.

---
+++ clasenodocita.py
@@ -0,0 +1,7 @@
+class NodoCita:
+    def __init__(self, cita):
+    
+        self.cita = cita      # La cita almacenada en el nodo
+        self.siguiente = None  # Puntero al siguiente nodo en la lista
+
+
